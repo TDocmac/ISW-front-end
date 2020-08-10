4ae4863d00e9d600ac7ec2ebc5435eb99e0374b2
Benjamín <benjamin.camblor@sansano.usm.cl>
2020-08-10 02:51:47 -0400

    fix sillones post at sillonList.Service

--- src/services/sillonList.service.js
+++ src/services/sillonList.service.js
@@ -8,7 +8,7 @@ function show (sillonId){
     return api.get(`${basePath}/id=${sillonId}`)
 };
 function create (data){
-    return api.post(`${basePath}/sillon`,data)
+    return api.post(`${basePath}/sillones`,data)
 };
 
 export const sillonService ={
