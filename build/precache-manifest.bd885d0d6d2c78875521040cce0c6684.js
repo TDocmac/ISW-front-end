1f96a946a450d5c491454a45718eb1c818cf9e99
TDocmac <jtdocmac@uc.cl>
2020-08-10 00:20:11 -0400

    Add files via upload

---
+++ build/precache-manifest.bd885d0d6d2c78875521040cce0c6684.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04459e63ca7356c214ee041f57fa8e95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59befccc7a13047003af",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c19c8ecc1fd5e2f86948",
+    "url": "/static/css/main.001da78b.chunk.css"
+  },
+  {
+    "revision": "59befccc7a13047003af",
+    "url": "/static/js/2.4df23d69.chunk.js"
+  },
+  {
+    "revision": "865f46d05fc3c49c93898fd56e06d864",
+    "url": "/static/js/2.4df23d69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c19c8ecc1fd5e2f86948",
+    "url": "/static/js/main.9dd06329.chunk.js"
+  },
+  {
+    "revision": "4af4206fafe13330f3c0",
+    "url": "/static/js/runtime-main.94605c97.js"
+  }
+]);
