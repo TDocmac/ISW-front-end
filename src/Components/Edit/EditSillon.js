662a8bd7bc3f9563d7801dd927b687f14287882c
Benjamín <benjamin.camblor@sansano.usm.cl>
2020-08-11 01:38:00 -0400

    include missing imports

--- src/Components/Edit/EditSillon.js
+++ src/Components/Edit/EditSillon.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import {sillonService} from '../../services'
+import {Form,Button,FormCheck,} from 'react-bootstrap'
 import axios from 'axios';
 import {Link} from 'react-router-dom';
 
